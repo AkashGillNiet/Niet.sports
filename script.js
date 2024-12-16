2747c6320c1f502f4ed0c75ad73d483b13f9fc91
AkashGillNiet <24862486gill@gmail.com>
2024-12-16 10:33:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-0
-
 document.addEventListener('DOMContentLoaded', function() {
-
     // Select all channel cards
-
     const channelCards = document.querySelectorAll('.channel-card');
 
     // Iterate through each channel card and add a click event listener
-
     channelCards.forEach(function(card) {
-
         card.addEventListener('click', function() {
-
             // Get the data-stream attribute value which contains the URL of the stream page
-
             const streamPage = card.getAttribute('data-stream');
 
             // Open the stream page in a new tab
-
             window.open(streamPage, '_blank');
-
         });
-
     });
-
 });
